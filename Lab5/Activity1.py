17de2119efe1c08a3dc1097cdee77254ec7603b3
Haris Ali <ali2haris3@gmail.com>
2024-03-11 11:53:21 +0500

    newadded

---
+++ Lab5/Activity1.py
@@ -0,0 +1,28 @@
+#  Node 0 has a value of 0 and has multiple children: nodes 1, 3, 6, 8, 10, 12, 14, 16, 17, 19, 22, and 25.
+#     Node 1 has a value of 1 and has one child: node 2.
+#     Node 3 has a value of 3 and has two children: nodes 4 and 5.
+#     Node 6 has a value of 6 and has one child: node 7.
+#     Node 8 has a value of 8 and has one child: node 9.
+#     Node 10 has a value of 10 and has one child: node 11.
+#     Node 12 has a value of 12 and has one child: node 13.
+#     Node 14 has a value of 14 and has one child: node 15.
+#     Node 16 has a value of 16 and has no children.
+#     Node 17 has a value of 17 and has one child: node 18.
+#     Node 19 has a value of 19 and has two children: nodes 20 and 21.
+#     Node 22 has a value of 22 and has two children: nodes 23 and 24.
+#     Node 25 has a value of 25 and has one child: node 5.
+dictionary = {
+    0: [1, 3, 6, 8, 10, 12, 14, 16, 17, 19, 22, 25],
+    1: [2],
+    3: [4, 5],
+    6: [7],
+    8: [9],
+    10: [11],
+    12: [13],
+    14: [15],
+    16: [],
+    17: [18],
+    19: [20, 21],
+    22: [23, 24],
+    25: [5]
+}
